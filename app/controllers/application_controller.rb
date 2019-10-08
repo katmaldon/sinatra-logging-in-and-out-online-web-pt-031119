9527b6e91ec1f3cc6f921653e2dc4c8d1176e4ba
Kat Maldon <katharinamaldon@gmail.com>
2019-10-08 17:27:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions unless test?
     set :session_secret, "secret"
+  end 
 
   get '/' do
     erb :index
